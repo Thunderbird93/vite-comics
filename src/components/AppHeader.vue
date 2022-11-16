5fe03e32890ac6011e9a4363ecebd7dfba32dd48
Thunderbird93 <antircristian@gmail.com>
2022-11-16 15:03:11 +0100

    [add] header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,9 +6,27 @@ export default {
 
 
 <template>
-    <header>Header</header>
+    <header>
+        <div class="logo">
+            <img src="/dc-logo.png" alt="logo">
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
 
-<style>
+<style lang="scss">
 
 </style>
