60c221ad3398fe189bf4e9c30536f3613e52181f
Thunderbird93 <antircristian@gmail.com>
2022-11-16 21:21:08 +0100

    [add] footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,9 +5,30 @@ export default {
 </script>
 
 <template>
-  <footer>Footer</footer>
+  <footer>
+    <div class="container">
+      <div class="cta">
+        <p>SIGN-UP NOW!</p>
+      </div>
+      <div class="social">
+        <p>FOLLOW US</p>
+        <img src="../assets/img/footer-facebook.png" alt="">
+      </div>
+    </div>
+  </footer>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use '../style/partial/mixin' as *;
+@use '../style/partial/variables' as *;
 
+footer{
+  background-color: #303030;
+  .container{
+    height: 120px;
+    //debug
+    border: 1px dotted red;
+  }
+}
+  
 </style>
