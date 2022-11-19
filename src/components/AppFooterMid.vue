6cb9b1476527bf1d43fa1666fa40451112c49fee
Thunderbird93 <antircristian@gmail.com>
2022-11-19 16:31:56 +0100

    [add] footer middle part

---
+++ src/components/AppFooterMid.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'AppFooterMid',
+    data(){
+        return
+    }
+}
+</script>
+
+
+<template>
+<div class="mid-foot">
+
+    <div class="container">
+        <div class="f-menu"></div>
+        <div class="f-logo"></div>
+    </div>
+</div>  
+    </template>
+
+
+<style lang="scss" scoped>
+@use '../style/partial/mixin' as *;
+@use '../style/partial/variables' as *;
+
+.mid-foot{
+background-image: url(../assets/img/footer-bg.jpg);
+height: 370px;
+}
+</style>
